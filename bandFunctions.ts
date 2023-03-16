f27ef0c6c3852fec9d3effddc9d0c81a7746232a
HugoJL <hugojl@gmail.com>
2023-03-16 05:23:26 +0200

    first commit

---
+++ bandFunctions.ts
@@ -0,0 +1,92 @@
+interface Member {
+    name: string;
+    age: number;
+    plays: string[];
+  }
+  
+  interface Members {
+    current: Member[];
+    past: Member[];
+  }
+  
+  interface Plays {
+    [play: string]: string[];
+  }
+  
+  interface NewBandData {
+    members: Members;
+    all: string[];
+    plays: Plays;
+  }
+  
+  function transformBandData(band: { members: Members }): NewBandData {
+    const allMembers = [...band.members.current, ...band.members.past];
+  
+    // lowercase and sort all members by age (DESC) and name (ASC)
+    const allMembersLowerCase = allMembers.map((member) =>
+      member.name.toLowerCase()
+    );
+    allMembersLowerCase.sort((a, b) => {
+      const memberA: Member | undefined = allMembers.find((member) =>
+        member.name.toLowerCase().includes(a)
+      );
+      const memberB = allMembers.find((member) =>
+        member.name.toLowerCase().includes(b)
+      );
+      if (!memberA || !memberB) {
+        return 0;
+      }
+      if (memberA.age !== memberB.age) {
+        return memberB.age - memberA.age;
+      }
+      return memberA.name.localeCompare(memberB.name);
+    });
+  
+    // get unique plays
+    const plays: Plays = {};
+    allMembers.forEach((member) => {
+      member.plays.forEach((play) => {
+        if (!plays[play]) {
+          plays[play] = [];
+        }
+        plays[play].push(member.name.toLowerCase());
+      });
+    });
+  
+    // lowercase and sort member names for each play
+    Object.entries(plays).forEach(([play, members]) => {
+      members.sort();
+    });
+  
+    const all = allMembersLowerCase;
+  
+    return {
+        members: {
+          current: band.members.current,
+          past: band.members.past,
+        },
+        all,
+        plays
+      };
+  }
+  
+  // Data
+  const band = {
+    members: {
+      current: [
+        { name: 'Sascha', age: 59, plays: ['vocals', 'synth', 'guitar', 'bass'] },
+        { name: 'Lucia', age: 49, plays: ['vocals', 'synth'] },
+        { name: 'Jules', age: 53, plays: ['guitar', 'bass', 'synth'] },
+        { name: 'Steve', age: 55, plays: ['guitar'] },
+      ],
+      past: [
+        { name: 'Raymond', age: 57, plays: ['vocals', 'synth'] },
+        { name: 'En', age: 52, plays: ['vocals', 'drums', 'guitar', 'synth'] },
+        { name: 'Gunter', age: 57, plays: ['guitar', 'synth'] },
+      ],
+    },
+  };
+  
+  const newBandData = transformBandData(band);
+  console.log("Transformed array: ", JSON.stringify(newBandData, null, 2));
+  
